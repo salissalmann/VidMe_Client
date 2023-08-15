6d9cba3badb62d1fbfa8868d11a700dac6d32543
Salis Salman <112612817+salissalmann@users.noreply.github.com>
2023-08-16 03:11:46 +0500

    Posts Added

--- src/Components/SeekerDashboard/Main.jsx
+++ src/Components/SeekerDashboard/Main.jsx
@@ -6,9 +6,80 @@ import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import ExploreIcon from '@mui/icons-material/Explore';
 import SettingsIcon from '@mui/icons-material/Settings';
 import WorkspacesIcon from '@mui/icons-material/Workspaces';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
+import InsertLinkIcon from '@mui/icons-material/InsertLink';
+import ImageIcon from '@mui/icons-material/Image';
+import SwitchVideoIcon from '@mui/icons-material/SwitchVideo';
 
+import { notification } from 'antd'
 export default function Main() {
+
+    const [postDescription, setdescription] = useState("")
+    const [image, setImage] = useState(null);
+    const [video, setVideo] = useState(null);
+    const [file, setFile] = useState(null);
+
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+    }
+
+    const handleVideoChange = (e) => {
+        if (e.target.files[0].type !== "video/mp4") {
+            notification.error({
+                message: "Error",
+                description: "Please upload a mp4 file"
+            })
+            return;
+        }
+        setVideo(e.target.files[0]);
+    }
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file.type !== "application/pdf") {
+            notification.error({
+                message: "Error",
+                description: "Please upload a pdf file"
+            })
+            return;
+        }
+        setFile(file);
+    };
+
+    const Post = async () => {
+        if (postDescription === "") {
+            notification.error({
+                message: "Error",
+                description: "Please enter a description"
+            })
+            return;
+        }
+        const Reponse =
+            await fetch("http://localhost:4002/create-post", {
+                method: "POST",
+                headers: {
+                    "Authorization-Token": localStorage.getItem('token'),
+                    "Content-Type": "application/json"
+                },
+                body:
+                    JSON.stringify({
+                        PostText: postDescription,
+                    }),
+            });
+        const content = await Reponse.json();
+        if (content.Success) {
+            notification.success({
+                message: "Success",
+                description: "Posted Successfully"
+            })
+        }
+        else {
+            notification.error({
+                message: "Error",
+                description: content.Message
+            })
+        }
+    }
+
     return (
         <>
             <NavigationBar />
@@ -36,7 +107,7 @@ export default function Main() {
                             <div className={styles.NavigationSideBarDiv}>
                                 <WorkspacesIcon className={styles.NavIcon} />
                                 <h3>SPACES</h3>
-                            </div> 
+                            </div>
                             <div className={styles.NavigationSideBarDiv}>
                                 <PeopleAltIcon className={styles.NavIcon} />
                                 <h3>CONNECTIONS</h3>
@@ -45,18 +116,68 @@ export default function Main() {
                                 <SettingsIcon className={styles.NavIcon} />
                                 <h3>SETTINGS</h3>
                             </div>
-                        </div>      
+                        </div>
                     </div>
                     <div className={styles.DashboardContainer2}>
                         <div className={styles.AddPost}>
                             <div className={styles.AddPostDiv}>
                                 <img src="/StaticImages/MockProfile.jpeg" alt="profile" className='NavProfileImage' />
-                                <input type="text" placeholder="What's on your mind?" />
+                                <textarea type="text" placeholder="What's on your mind?"
+                                    onChange={(e) => setdescription(e.target.value)} value={postDescription}
+                                />
                             </div>
-                            <button className={styles.PostButton}>
-                                Create Post !
-                            </button>
+                            <div className={styles.AddPostDiv2}>
+                                <div className={styles.AddPostDiv2Inner}>
+                                    <div className={styles.AddPostDiv2InnerDiv} onClick={() => [document.getElementById('file1').click()]}>
+                                        <input type="file" id="file1" onChange={handleImageChange} hidden
+
+                                        />
+                                        <ImageIcon className={styles.UploadIcon}
+                                        /> Add Image
+                                    </div>
+                                    <div className={styles.AddPostDiv2InnerDiv} onClick={() => [document.getElementById('file2').click()]}>
+                                        <input type="file" id="file2" onChange={handleVideoChange} hidden
+
+                                        />
+                                        <SwitchVideoIcon className={styles.UploadIcon} /> Add Video
+                                    </div>
+                                    <div className={styles.AddPostDiv2InnerDiv} onClick={() => [document.getElementById('file3').click()]}>
+                                        <input type="file" id="file3" onChange={handleFileChange} hidden />
+                                        <InsertLinkIcon className={styles.UploadIcon} /> Add File
+                                    </div>
+                                    {image &&
+                                        <h6>
+                                            {image.name}
+                                        </h6>
+                                    }
+                                    {video &&
+                                        <h6>
+                                            {video.name}
+                                        </h6>
+                                    }
+                                    {file &&
+                                        <h6>
+                                            {file.name}
+                                        </h6>
+                                    }
+                                </div>
+                                <button className={styles.PostButton}
+                                    onClick={Post}>
+                                    Create Post !
+                                </button>
+                            </div>
                         </div>
+                        
+
+                        <div className={styles.PostDisplay}>
+                            
+                        </div>
+
+
+                        
+
+
+
                     </div>
                     <div className={styles.DashboardContainer3}>
                         <div className={styles.DashboardDiv4}>
